3990be23052ffe92e75cb566fcf8a6cdf2d72ced
Ravi <rksnhes1999@gmail.com>
2022-12-12 13:05:29 +0530

    code again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,14 +6,14 @@ public class Main {
      B b = new B();
      b.meth();
   }
-  public class A
+  public static class A
   {
       public String meth() {
           return "Invoking method from class A";
       }
   }
 
- public class B extends A{
+ public static class B extends A{
      @Override
       public String meth() {
          return "Method is overridden in Extendend class B";
